038842e07388bc7668787bcd50f44501e2a4f39e
Mark Perry <mark.j.perry11@gmail.com>
2016-01-12 16:53:14 -0800

    Add back message temporary variable string to spec

--- spec/solitaire_spec.rb
+++ spec/solitaire_spec.rb
@@ -3,9 +3,10 @@ require_relative '../solitaire'
 
 describe Solitaire,"#encrypt" do
   it 'successfully encrypts message with unkeyed deck' do
+    message = 'Code in Ruby, live longer!'
     expected_output = 'GLNCQ MJAFF FVOMB JIYCB'
 
-    encrypted_message = Solitaire.new(message: 'Code in Ruby, live longer!').encrypt
+    encrypted_message = Solitaire.new(message: message).encrypt
 
     expect(encrypted_message).to eq expected_output
   end
@@ -13,9 +14,10 @@ end
 
 describe Solitaire, "#decrypt" do
   it 'successfully decrypts message' do
+    encrypted_message = 'GLNCQ MJAFF FVOMB JIYCB'
     expected_output = "CODEI NRUBY LIVEL ONGER"
 
-    decrypted_message = Solitaire.new(encrypted_message: 'GLNCQ MJAFF FVOMB JIYCB').decrypt
+    decrypted_message = Solitaire.new(encrypted_message: encrypted_message).decrypt
 
     expect(decrypted_message).to eq expected_output
   end
